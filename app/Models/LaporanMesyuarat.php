552cdc647a2a08e5c1c1fae5177acc2bc0b7ed0b
laporanpuunegeri <laporanpuunegeri@gmail.com>
2025-05-21 11:57:13 +0800

    Update LaporanMesyuarat.php

--- app/Models/LaporanMesyuarat.php
+++ app/Models/LaporanMesyuarat.php
@@ -4,18 +4,34 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class LaporanMesyuarat extends Model
 {
     use HasFactory;
 
-    protected $table = 'laporan_mesyuarats'; // pastikan padan dengan nama jadual
+    protected $table = 'laporan_mesyuarats';
 
     protected $fillable = [
         'mesyuarat',
         'isu',
         'tarikh_mesyuarat',
         'status',
-        'pandangan'
+        'pandangan',
+        'user_id',
+        'negeri',
+        'created_at',
+        'updated_at',
     ];
+
+    protected $casts = [
+        'tarikh_mesyuarat' => 'date',
+        'created_at' => 'datetime',
+        'updated_at' => 'datetime',
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
