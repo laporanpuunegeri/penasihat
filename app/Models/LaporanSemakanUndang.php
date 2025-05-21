c2439398436420ee757db17316f9a9eacec4e1d7
laporanpuunegeri <laporanpuunegeri@gmail.com>
2025-05-21 12:00:05 +0800

    Update LaporanSemakanUndang.php

--- app/Models/LaporanSemakanUndang.php
+++ app/Models/LaporanSemakanUndang.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class LaporanSemakanUndang extends Model
 {
@@ -15,5 +16,19 @@ class LaporanSemakanUndang extends Model
         'tajuk',
         'tindakan',
         'status',
+        'user_id',
+        'negeri',
+        'created_at',
+        'updated_at',
     ];
+
+    protected $casts = [
+        'created_at' => 'datetime',
+        'updated_at' => 'datetime',
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
