b38224789b8c48f85c797a349c1eaa42be092b31
laporanpuunegeri <laporanpuunegeri@gmail.com>
2025-05-21 11:52:06 +0800

    Update LainLainTugasan.php

--- app/Models/LainLainTugasan.php
+++ app/Models/LainLainTugasan.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class LainLainTugasan extends Model
 {
@@ -15,7 +16,18 @@ class LainLainTugasan extends Model
         'perihal',
         'tarikh',
         'tindakan',
+        'user_id',
+        'negeri',
+    ];
+
+    protected $casts = [
+        'tarikh' => 'date',
     ];
 
     public $timestamps = true;
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
