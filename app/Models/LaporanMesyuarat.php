aede8239e776af5c689efe2b491230e03656ea92
laporanpuunegeri <laporanpuunegeri@gmail.com>
2025-05-23 21:14:32 +0800

    Update LaporanMesyuarat.php

--- app/Models/LaporanMesyuarat.php
+++ app/Models/LaporanMesyuarat.php
@@ -32,6 +32,8 @@ class LaporanMesyuarat extends Model
 
     public function user(): BelongsTo
     {
-        return $this->belongsTo(User::class);
+        return $this->belongsTo(User::class)->withDefault([
+            'name' => '-'
+        ]);
     }
 }
